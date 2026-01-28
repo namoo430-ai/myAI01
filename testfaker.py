53e12d7b768b6d0c1d1b589503213c23ccd88258
yong <namoo430@gmail.com>
2026-01-28 12:44:50 +0900

    2026-01-28 1

---
+++ testfaker.py
@@ -0,0 +1,44 @@
+# 에러 ModuleNotFoundError: No module named 'qrcode'
+# C:\dongaAI\day0123> pip install qrcode
+
+# 에러 ModuleNotFoundError: No module named 'cv2'
+# 에러 C:\dongaAI\day0123> pip install cv2
+# 정답 C:\dongaAI\day0123> pip install opencv-python
+
+# 에러 ModuleNotFoundError: No module named 'faker'
+# C:\dongaAI\day0123> pip install faker 
+
+# testfaker.py
+
+import qrcode
+import cv2
+from faker import Faker
+
+my = Faker()
+for k in range(10):
+    print(my.name())
+
+print()
+for k in range(10):
+    print(my.ipv4_private())
+
+
+print()
+for k in range(10):
+    print(my.ipv4_private())
+
+data = Faker('ko_KR')
+
+
+
+
+
+# 에러 ModuleNotFoundError: No module named 'qrcode'
+# C:\dongaAI\day0123> pip install qrcode
+
+# 에러 ModuleNotFoundError: No module named 'cv2'
+# 에러 C:\dongaAI\day0123> pip install cv2
+# 정답 C:\dongaAI\day0123> pip install opencv-python
+
+# 에러 ModuleNotFoundError: No module named 'faker'
+# C:\dongaAI\day0123> pip install faker 
